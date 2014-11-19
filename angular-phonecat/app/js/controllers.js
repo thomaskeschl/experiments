a05caff383670ec472692f2a9127f7e17bf3bbb5
Tom Keschl <thomas.keschl@gmail.com>
2014-11-18 21:20:26 -0500

    Add step 2 tasks and experiments.

--- angular-phonecat/app/js/controllers.js
+++ angular-phonecat/app/js/controllers.js
@@ -1,3 +1,17 @@
 'use strict';
 
 /* Controllers */
+
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function ($scope) {
+    $scope.phones = [
+        {'name':'Nexus S',
+         'snippet':'Fast just got faster with Nexus S'},
+        {'name':'Motorola XOOM with WiFi',
+         'snippet':'The Next, Next Generation tablet.'},
+        {'name':'Motorola XOOM',
+         'snippet':'The Next, Next Generation tablet.'}
+    ];
+    $scope.name = "World!"
+});
